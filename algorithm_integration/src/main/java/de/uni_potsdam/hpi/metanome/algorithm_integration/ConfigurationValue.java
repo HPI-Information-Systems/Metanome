9db9239de6b6ec74ff61c5e62fc02cf43cb87c67
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-07-24 14:20:40 +0200

    made certain interfaces and abstract classes public so that they can be used in loader put result receivers in seperate package

--- algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/ConfigurationValue.java
+++ algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/ConfigurationValue.java
@@ -4,7 +4,7 @@ package de.uni_potsdam.hpi.metanome.algorithm_integration;
  * Represents configuration values for {@link Algorithm}s. Sets it's own value on an {@link Algorithm}
  * through double dispatch (second call).
  */
-interface ConfigurationValue {
+public interface ConfigurationValue {
 	
 	/**
 	 * Sets the configuration value on the algorithm. 
