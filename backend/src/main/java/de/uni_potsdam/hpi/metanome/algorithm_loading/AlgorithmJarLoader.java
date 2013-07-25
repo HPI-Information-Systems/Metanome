20294f00260cb6e6cdd2093a4bff6bbea8aff65f
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-07-25 10:25:01 +0200

    added AlgorithmJarLoader

---
+++ backend/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_loading/AlgorithmJarLoader.java
@@ -0,0 +1,40 @@
+package de.uni_potsdam.hpi.metanome.algorithm_loading;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+import de.uni_potsdam.hpi.metanome.algorithm_integration.Algorithm;
+
+public class AlgorithmJarLoader<T extends Algorithm> {
+	
+	protected static final String bootstrapClassTagName = "Algorithm-Bootstrap-Class";
+	protected Class<T> algorithmSubclass;
+	
+	public AlgorithmJarLoader(Class<T> algorithmSubclass) {
+		this.algorithmSubclass = algorithmSubclass;
+	}
+	
+	public Algorithm loadAlgorithm(String path) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+		File file = new File(path);
+		JarFile jar = new JarFile(file);
+		
+		Manifest man = jar.getManifest();
+        Attributes attr = man.getMainAttributes();
+        String className = attr.getValue(bootstrapClassTagName);
+        
+        URL[] url = {file.toURI().toURL()};
+        
+        ClassLoader loader = new URLClassLoader(url);
+        
+        Class<? extends T> algorithmClass = 
+        		Class.forName(className, true, loader).asSubclass(algorithmSubclass);
+        
+		return algorithmClass.newInstance();
+	}
+	
+}
---
+++ backend/src/test/java/de/uni_potsdam/hpi/metanome/algorithm_loading/AlgorithmJarLoaderTest.java
@@ -0,0 +1,53 @@
+
+package de.uni_potsdam.hpi.metanome.algorithm_loading;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uni_potsdam.hpi.metanome.algorithm_integration.Algorithm;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.UniqueColumnCombinationsAlgorithm;
+
+/**
+
+ *
+ */
+public class AlgorithmJarLoaderTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void loadAlgorithm() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+		// Setup
+		AlgorithmJarLoader<UniqueColumnCombinationsAlgorithm> loader = 
+				new AlgorithmJarLoader<UniqueColumnCombinationsAlgorithm>(UniqueColumnCombinationsAlgorithm.class);
+		String jarFilePath = ClassLoader.getSystemResource("testjar.jar").getFile();
+		
+		// Execute functionality
+		Algorithm algorithm = loader.loadAlgorithm(jarFilePath);
+		
+		// Check result
+		assertNotNull(algorithm);
+		assertTrue(algorithm instanceof UniqueColumnCombinationsAlgorithm);		
+	}
+
+}
