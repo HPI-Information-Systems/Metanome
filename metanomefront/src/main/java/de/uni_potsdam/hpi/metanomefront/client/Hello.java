b129030e14d7428299ee167fd347922a3059a8a6
Claudia Exeler <cl.ex@gmx.de>
2013-07-26 13:09:01 +0200

    clean up imports

--- metanomefront/src/main/java/de/uni_potsdam/hpi/metanomefront/client/Hello.java
+++ metanomefront/src/main/java/de/uni_potsdam/hpi/metanomefront/client/Hello.java
@@ -1,30 +1,12 @@
-/*
- * Copyright 2007 Google Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package de.uni_potsdam.hpi.metanomefront.client;
 
 import java.util.ArrayList;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
-import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import de.uni_potsdam.hpi.metanomefront.server.InputParameter;
--- metanomefront/src/main/java/de/uni_potsdam/hpi/metanomefront/client/JarChooser.java
+++ metanomefront/src/main/java/de/uni_potsdam/hpi/metanomefront/client/JarChooser.java
@@ -2,7 +2,6 @@ package de.uni_potsdam.hpi.metanomefront.client;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
