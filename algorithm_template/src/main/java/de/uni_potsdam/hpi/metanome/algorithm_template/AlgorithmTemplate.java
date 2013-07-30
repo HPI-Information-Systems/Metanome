00025cf943a0e41586f41a8786361fe4c7a8fb20
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-07-30 20:16:53 +0200

    added AlgorithmTemplate

---
+++ algorithm_template/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_template/AlgorithmTemplate.java
@@ -0,0 +1,29 @@
+package de.uni_potsdam.hpi.metanome.algorithm_template;
+
+import java.util.List;
+
+import de.uni_potsdam.hpi.metanome.algorithm_integration.UniqueColumnCombinationsAlgorithm;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecification;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.UniqueColumnCombinationResultReceiver;
+
+public class AlgorithmTemplate extends UniqueColumnCombinationsAlgorithm {
+
+	@Override
+	public List<ConfigurationSpecification> getConfigurationRequirements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setConfigurationValue(String identifier, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start(UniqueColumnCombinationResultReceiver resultReceiver) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
