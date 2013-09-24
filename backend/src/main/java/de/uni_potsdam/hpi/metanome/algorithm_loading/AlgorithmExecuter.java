3bf3965aa7711caf76994f98c03549311286629f
Claudia Exeler <cl.ex@gmx.de>
2013-09-24 17:03:22 +0200

    Add algorithm execution test and code

--- backend/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_loading/AlgorithmExecuter.java
+++ backend/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_loading/AlgorithmExecuter.java
@@ -2,25 +2,82 @@ package de.uni_potsdam.hpi.metanome.algorithm_loading;
 
 import java.util.List;
 
+import de.uni_potsdam.hpi.metanome.algorithm_integration.FunctionalDependencyAlgorithm;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.InclusionDependencyAlgorithm;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.UniqueColumnCombinationsAlgorithm;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationValue;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.FunctionalDependencyResultReceiver;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.InclusionDependencyResultReceiver;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.UniqueColumnCombinationResultReceiver;
 
 public class AlgorithmExecuter {
 
+	/**
+	 * 
+	 * @param algorithmName
+	 * @param configs
+	 * @param resultReceiver
+	 */
 	public void executeInclusionDependencyAlgorithm(String algorithmName,
-			List<ConfigurationValue> configs) {
-		// TODO load, configure & run the algorithm
+			List<ConfigurationValue> configs, InclusionDependencyResultReceiver resultReceiver) {
+		AlgorithmJarLoader<InclusionDependencyAlgorithm> loader = 
+				new AlgorithmJarLoader<InclusionDependencyAlgorithm>(InclusionDependencyAlgorithm.class);
+		InclusionDependencyAlgorithm algorithm = null;
 		
+		try {
+			algorithm = loader.loadAlgorithm(algorithmName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		algorithm.configure(configs);
+		algorithm.start(resultReceiver);		
 	}
 
+	/**
+	 * 
+	 * @param algorithmName
+	 * @param configs
+	 * @param resultReceiver
+	 */
 	public void executeFunctionalDependencyAlgorithm(String algorithmName,
-			List<ConfigurationValue> configs) {
-		// TODO load, configure & run the algorithm
+			List<ConfigurationValue> configs, FunctionalDependencyResultReceiver resultReceiver) {
+		AlgorithmJarLoader<FunctionalDependencyAlgorithm> loader = 
+				new AlgorithmJarLoader<FunctionalDependencyAlgorithm>(FunctionalDependencyAlgorithm.class);
+		FunctionalDependencyAlgorithm algorithm = null;
 		
+		try {
+			algorithm = loader.loadAlgorithm(algorithmName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		algorithm.configure(configs);
+		algorithm.start(resultReceiver);
 	}
 
+	/**
+	 * 
+	 * @param algorithmName
+	 * @param configs
+	 * @param resultReceiver
+	 */
 	public void executeUniqueColumnCombinationsAlgorithm(String algorithmName,
-			List<ConfigurationValue> configs) {
-		// TODO load, configure & run the algorithm
+			List<ConfigurationValue> configs, UniqueColumnCombinationResultReceiver resultReceiver) {
+		AlgorithmJarLoader<UniqueColumnCombinationsAlgorithm> loader = 
+				new AlgorithmJarLoader<UniqueColumnCombinationsAlgorithm>(UniqueColumnCombinationsAlgorithm.class);
+		UniqueColumnCombinationsAlgorithm algorithm = null;
 		
+		try {
+			algorithm = loader.loadAlgorithm(algorithmName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		algorithm.configure(configs);
+		algorithm.start(resultReceiver);
 	}
 }
