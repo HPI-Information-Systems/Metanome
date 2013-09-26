ce090cd9f6429aecbf689494ff7b14a98add0d81
Claudia Exeler <cl.ex@gmx.de>
2013-09-26 15:39:28 +0200

    add execution service including test

--- frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/server/ExecutionServiceImpl.java
+++ frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/server/ExecutionServiceImpl.java
@@ -1,13 +1,22 @@
 package de.uni_potsdam.hpi.metanome.frontend.server;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationValue;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationValueString;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.FunctionalDependencyResultReceiver;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.InclusionDependencyResultReceiver;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.UniqueColumnCombinationResultReceiver;
 import de.uni_potsdam.hpi.metanome.algorithm_loading.AlgorithmExecuter;
 import de.uni_potsdam.hpi.metanome.frontend.client.parameter.InputParameter;
+import de.uni_potsdam.hpi.metanome.frontend.client.parameter.InputParameterString;
 import de.uni_potsdam.hpi.metanome.frontend.client.services.ExecutionService;
+import de.uni_potsdam.hpi.metanome.result_receiver.UniqueColumnCombinationPrinter;
 
 /**
  * Service Implementation for service that triggers algorithm execution
@@ -21,8 +30,23 @@ public class ExecutionServiceImpl extends RemoteServiceServlet implements
 	
 	private List<ConfigurationValue> convertInputParameters(
 			List<InputParameter> parameters) {
-		// TODO Auto-generated method stub
-		return null;
+		List<ConfigurationValue> configValuesList = new LinkedList<ConfigurationValue>();
+		for (InputParameter parameter : parameters){
+			ConfigurationValue configValue = convertToConfigurationValue(parameter);
+			configValuesList.add(configValue);
+		}
+		return configValuesList;
+	}
+
+
+	private ConfigurationValue convertToConfigurationValue(
+			InputParameter parameter) {
+		//TODO different types of ConfigurationValues
+		if (parameter instanceof InputParameterString)
+			return new ConfigurationValueString(parameter.getIdentifier(), 
+					(String) parameter.getValue());
+		else
+			return null;
 	}
 	
 
@@ -30,8 +54,9 @@ public class ExecutionServiceImpl extends RemoteServiceServlet implements
 	public void executeInclusionDependencyAlgorithm(String algorithmName,
 			List<InputParameter> parameters) {
 		List<ConfigurationValue> configs = convertInputParameters(parameters);
-		executer.executeInclusionDependencyAlgorithm(algorithmName, configs);
-		// TODO retrieve and forward the result
+		InclusionDependencyResultReceiver resultReceiver = null; //TODO instantiate
+		
+		executer.executeInclusionDependencyAlgorithm(algorithmName, configs, resultReceiver);
 		
 	}
 
@@ -39,8 +64,9 @@ public class ExecutionServiceImpl extends RemoteServiceServlet implements
 	public void executeFunctionalDependencyAlgorithm(String algorithmName,
 			List<InputParameter> parameters) {
 		List<ConfigurationValue> configs = convertInputParameters(parameters);
-		executer.executeFunctionalDependencyAlgorithm(algorithmName, configs);
-		// TODO retrieve and forward the result
+		FunctionalDependencyResultReceiver resultReceiver = null; //TODO instantiate
+		
+		executer.executeFunctionalDependencyAlgorithm(algorithmName, configs, resultReceiver);
 		
 	}
 
@@ -48,9 +74,11 @@ public class ExecutionServiceImpl extends RemoteServiceServlet implements
 	public void executeUniqueColumnCombinationsAlgorithm(String algorithmName,
 			List<InputParameter> parameters) {
 		List<ConfigurationValue> configs = convertInputParameters(parameters);
-		executer.executeUniqueColumnCombinationsAlgorithm(algorithmName, configs);
-		// TODO retrieve and forward the result
+		UniqueColumnCombinationResultReceiver resultReceiver = new UniqueColumnCombinationPrinter(
+				new PrintStream(new ByteArrayOutputStream())); //TODO instantiate useful one
 		
+		executer.executeUniqueColumnCombinationsAlgorithm(algorithmName, configs, resultReceiver);
+		System.out.println("Successfully executed " + algorithmName);
 	}
 	
 
--- frontend/src/test/java/de/uni_potsdam/hpi/metanome/frontend/client/ServiceCallTest.java
+++ frontend/src/test/java/de/uni_potsdam/hpi/metanome/frontend/client/ServiceCallTest.java
@@ -1,5 +1,6 @@
 package de.uni_potsdam.hpi.metanome.frontend.client;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -9,6 +10,9 @@ import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import de.uni_potsdam.hpi.metanome.frontend.client.parameter.InputParameter;
+import de.uni_potsdam.hpi.metanome.frontend.client.parameter.InputParameterString;
+import de.uni_potsdam.hpi.metanome.frontend.client.services.ExecutionService;
+import de.uni_potsdam.hpi.metanome.frontend.client.services.ExecutionServiceAsync;
 import de.uni_potsdam.hpi.metanome.frontend.client.services.FinderService;
 import de.uni_potsdam.hpi.metanome.frontend.client.services.FinderServiceAsync;
 import de.uni_potsdam.hpi.metanome.frontend.client.services.ParameterService;
@@ -19,29 +23,62 @@ import de.uni_potsdam.hpi.metanome.frontend.client.services.ParameterServiceAsyn
  */
 public class ServiceCallTest extends GWTTestCase {
     
-//	public void testParameterService(){
-//		//Setup
-//		AsyncCallback<List<InputParameter>> callback = new AsyncCallback<List<InputParameter>>() {
-//		      public void onFailure(Throwable caught) {
-//		    	  fail();
-//		      }
-//
-//		      public void onSuccess(List<InputParameter> result) {  	  
-//		    	  assertNotNull(result);
-//		    	  finishTest();
-//		      }
-//		    };
-//
-//		  ParameterServiceAsync parameterService = GWT.create(ParameterService.class);
-//		  
-//		  
-//		  // Set a delay period
-//		  delayTestFinish(500);
-//
-//		  //Execute
-//		  parameterService.retrieveUniqueColumnCombinationsParameters("testjar.jar", callback);
-//
-//	}
+	@Test
+	public void testExecutionService(){
+		//Setup
+		List<InputParameter> configs = new ArrayList<InputParameter>();
+		InputParameterString inputParameter = new InputParameterString("pathToInputFile");
+		inputParameter.setValue("blub");
+		configs.add(inputParameter);
+		
+		AsyncCallback<Void> callback = new AsyncCallback<Void>() {
+		      public void onFailure(Throwable caught) {
+		    	  fail();
+		      }
+
+		      public void onSuccess(Void result) {  	  
+		    	  finishTest();
+		      }
+		    };
+
+		  ExecutionServiceAsync executionService = GWT.create(ExecutionService.class);
+		  
+		  
+		  // Set a delay period
+		  delayTestFinish(500);
+
+		  //Execute
+		  executionService.executeUniqueColumnCombinationsAlgorithm(
+				  "example_algorithm-0.0.1-SNAPSHOT-jar-with-dependencies.jar", 
+				  configs, callback);
+
+	}
+	
+	@Test
+	public void testParameterService(){
+		//Setup
+		AsyncCallback<List<InputParameter>> callback = new AsyncCallback<List<InputParameter>>() {
+		      public void onFailure(Throwable caught) {
+		    	  fail();
+		      }
+
+		      public void onSuccess(List<InputParameter> result) {  	  
+		    	  assertNotNull(result);
+		    	  finishTest();
+		      }
+		    };
+
+		  ParameterServiceAsync parameterService = GWT.create(ParameterService.class);
+		  
+		  
+		  // Set a delay period
+		  delayTestFinish(500);
+
+		  //Execute
+		  parameterService.retrieveUniqueColumnCombinationsParameters(
+				  "example_algorithm-0.0.1-SNAPSHOT-jar-with-dependencies.jar", callback);
+
+	}
 	
 	@Test
 	public void testFinderService(){
