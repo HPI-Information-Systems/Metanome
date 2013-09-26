d84356b21ecf593ea18d1d92c9ee6025993d8e27
Claudia Exeler <cl.ex@gmx.de>
2013-09-26 15:37:07 +0200

    add execution test

---
+++ backend/src/test/java/de/uni_potsdam/hpi/metanome/algorithm_loading/AlgorithmExecutionTest.java
@@ -0,0 +1,40 @@
+package de.uni_potsdam.hpi.metanome.algorithm_loading;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnCombination;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnIdentifier;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationValue;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationValueString;
+import de.uni_potsdam.hpi.metanome.result_receiver.UniqueColumnCombinationPrinter;
+
+public class AlgorithmExecutionTest {
+
+	@Test
+	public void executeUniqueColumnCombinationsTest() {
+		// Setup
+		AlgorithmExecuter executer = new AlgorithmExecuter();
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		List<ConfigurationValue> configs = new ArrayList<ConfigurationValue>();
+		configs.add(new ConfigurationValueString("pathToInputFile", "blub"));
+
+		// Expected values
+		ColumnCombination columnCombination = new ColumnCombination(
+				new ColumnIdentifier("table1", "column1"), 
+				new ColumnIdentifier("table2", "column2"));
+				
+		// Execute
+		executer.executeUniqueColumnCombinationsAlgorithm("example_algorithm-0.0.1-SNAPSHOT-jar-with-dependencies.jar", configs, 
+				new UniqueColumnCombinationPrinter(new PrintStream(outStream)));
+		
+		// Check result
+		assertTrue(outStream.toString().contains(columnCombination.toString()));
+	}
+}
