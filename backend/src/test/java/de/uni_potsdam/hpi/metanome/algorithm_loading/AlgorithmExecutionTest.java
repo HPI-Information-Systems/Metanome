567d22d325d4dfdcd1e6ec045c7586a6bed90c34
Claudia Exeler <cl.ex@gmx.de>
2013-10-02 14:40:44 +0200

    add execution tests and needed algorithm jars

--- backend/src/test/java/de/uni_potsdam/hpi/metanome/algorithm_loading/AlgorithmExecutionTest.java
+++ backend/src/test/java/de/uni_potsdam/hpi/metanome/algorithm_loading/AlgorithmExecutionTest.java
@@ -1,77 +1,78 @@
 package de.uni_potsdam.hpi.metanome.algorithm_loading;
 
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.mock;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnCombination;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnIdentifier;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationValue;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationValueString;
-import de.uni_potsdam.hpi.metanome.result_receiver.UniqueColumnCombinationPrinter;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.FunctionalDependencyResultReceiver;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.InclusionDependencyResultReceiver;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.UniqueColumnCombinationResultReceiver;
+import de.uni_potsdam.hpi.metanome.result_receiver.FunctionalDependencyFileWriter;
+import de.uni_potsdam.hpi.metanome.result_receiver.InclusionDependencyFileWriter;
+import de.uni_potsdam.hpi.metanome.result_receiver.UniqueColumnCombinationFileWriter;
 
 public class AlgorithmExecutionTest {
+	
+	private AlgorithmExecuter executer;
+	
+	@Before
+	public void setUp(){
+		executer = new AlgorithmExecuter();
+	}
 
 	@Test
 	public void executeUniqueColumnCombinationsAlgorithmTest() {
 		// Setup
-		AlgorithmExecuter executer = new AlgorithmExecuter();
-		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 		List<ConfigurationValue> configs = new ArrayList<ConfigurationValue>();
 		configs.add(new ConfigurationValueString("pathToInputFile", "blub"));
-
-		// Expected values
-		ColumnCombination columnCombination = new ColumnCombination(
-				new ColumnIdentifier("table1", "column1"), 
-				new ColumnIdentifier("table2", "column2"));
+		UniqueColumnCombinationResultReceiver resultReceiver = mock(UniqueColumnCombinationFileWriter.class);
 				
 		// Execute
 		executer.executeUniqueColumnCombinationsAlgorithm("example_algorithm-0.0.1-SNAPSHOT-jar-with-dependencies.jar", configs, 
-				new UniqueColumnCombinationPrinter(new PrintStream(outStream)));
+				resultReceiver);
 		
 		// Check result
-		assertTrue(outStream.toString().contains(columnCombination.toString()));
+		verify(resultReceiver).receiveResult(isA(ColumnCombination.class));
 	}
 	
 	@Test
 	public void executeFunctionalDependencyAlgorithmTest() {
 		// Setup
-		AlgorithmExecuter executer = new AlgorithmExecuter();
-		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 		List<ConfigurationValue> configs = new ArrayList<ConfigurationValue>();
-		configs.add(new ConfigurationValueString("pathToInputFile", "blub"));
-
-		// Expected values
-		ColumnCombination columnCombination = new ColumnCombination(
-				new ColumnIdentifier("table1", "column1"), 
-				new ColumnIdentifier("table2", "column2"));
+		configs.add(new ConfigurationValueString("pathToOutputFile", "blub"));
+		FunctionalDependencyResultReceiver resultReceiver = mock(FunctionalDependencyFileWriter.class);
 				
 		// Execute TODO
-//		executer.executeFunctionalDependencyAlgorithm("example_algorithm-0.0.1-SNAPSHOT-jar-with-dependencies.jar", configs, 
-//				null);
+		executer.executeFunctionalDependencyAlgorithm("example_fd_algorithm_0.0.1.jar", configs, 
+				resultReceiver);
 		
 		// Check result
-		//assertTrue(outStream.toString().contains(columnCombination.toString()));
+		verify(resultReceiver).receiveResult(isA(ColumnCombination.class), isA(ColumnIdentifier.class));
 	}
 	
 	@Test
 	public void executeInclusionDependencyTest() {
 		// Setup
-		AlgorithmExecuter executer = new AlgorithmExecuter();
-		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 		List<ConfigurationValue> configs = new ArrayList<ConfigurationValue>();
-		configs.add(new ConfigurationValueString("pathToInputFile", "blub"));
-				
+		configs.add(new ConfigurationValueString("tableName", "blub"));
+		InclusionDependencyResultReceiver resultReceiver = mock(InclusionDependencyFileWriter.class);		
+		
 		// Execute
-		// TODO executer.executeInclusionDependencyAlgorithm("example_algorithm-0.0.1-SNAPSHOT-jar-with-dependencies.jar", configs, 
-				//null);
+		executer.executeInclusionDependencyAlgorithm("example_ind_algorithm-0.0.1-SNAPSHOT-jar-with-dependencies.jar", configs, 
+				resultReceiver);
 		
 		// Check result
-		
+		verify(resultReceiver).receiveResult(isA(ColumnCombination.class), isA(ColumnCombination.class));
+
 	}
 }
