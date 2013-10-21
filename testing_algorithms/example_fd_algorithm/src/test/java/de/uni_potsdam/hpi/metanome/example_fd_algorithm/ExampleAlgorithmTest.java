188a5ac636754b431d297f9a038f84ab5ff784a9
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-10-21 18:38:29 +0200

    added fd example algorithm

---
+++ testing_algorithms/example_fd_algorithm/src/test/java/de/uni_potsdam/hpi/metanome/example_fd_algorithm/ExampleAlgorithmTest.java
@@ -0,0 +1,91 @@
+package de.uni_potsdam.hpi.metanome.example_fd_algorithm;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnCombination;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnIdentifier;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecification;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecificationString;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.CouldNotReceiveResultException;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.FunctionalDependencyResultReceiver;
+
+public class ExampleAlgorithmTest {
+
+	protected ExampleAlgorithm algorithm;
+	protected String pathIdentifier;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		algorithm = new ExampleAlgorithm();
+		pathIdentifier = "pathToInputFile";
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	/**
+	 * The algorithm should return one configuration specification of string type
+	 */
+	@Test
+	public void testGetConfigurationRequirements() {
+		// Execute functionality
+		List<ConfigurationSpecification> actualConfigurationRequirements = this.algorithm.getConfigurationRequirements();
+		
+		// Check result
+		assertEquals(2, actualConfigurationRequirements.size());
+		assertThat(actualConfigurationRequirements.get(0), instanceOf(ConfigurationSpecificationString.class));
+	}
+
+	/**
+	 * The algorithm should store the path when it is supplied through setConfigurationValue.
+	 */
+	@Test
+	public void testSetConfigurationValue() {
+		// Setup
+		// Expected values
+		String expectedConfigurationValue = "test";
+		
+		// Execute functionality
+		this.algorithm.setConfigurationValue(pathIdentifier, expectedConfigurationValue);
+		
+		// Check result
+		assertEquals(expectedConfigurationValue, this.algorithm.path);
+	}
+
+	/**
+	 * When the algorithm is started after configuration a result should be received.
+	 * @throws CouldNotReceiveResultException 
+	 */
+	@Test
+	public void testStart() throws CouldNotReceiveResultException {
+		// Setup
+		FunctionalDependencyResultReceiver resultReceiver = mock(FunctionalDependencyResultReceiver.class);
+		this.algorithm.setConfigurationValue(pathIdentifier, "something");
+		
+		// Execute functionality
+		this.algorithm.setResultReceiver(resultReceiver);
+		this.algorithm.start();
+		
+		// Check result
+		verify(resultReceiver).receiveResult(isA(ColumnCombination.class), isA(ColumnIdentifier.class));
+	}
+}
