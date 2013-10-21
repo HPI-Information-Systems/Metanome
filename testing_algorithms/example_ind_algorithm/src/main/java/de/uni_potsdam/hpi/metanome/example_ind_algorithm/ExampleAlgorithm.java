3a0ed56025cf41db50b34034f8879996ea6abc5e
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-10-21 18:07:18 +0200

    added example ind algorithm

---
+++ testing_algorithms/example_ind_algorithm/src/main/java/de/uni_potsdam/hpi/metanome/example_ind_algorithm/ExampleAlgorithm.java
@@ -0,0 +1,64 @@
+package de.uni_potsdam.hpi.metanome.example_ind_algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnCombination;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnIdentifier;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.InclusionDependencyAlgorithm;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecification;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecificationCsvFile;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecificationString;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.input.CsvFileGenerator;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.CouldNotReceiveResultException;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.InclusionDependencyResultReceiver;
+
+public class ExampleAlgorithm implements InclusionDependencyAlgorithm {
+
+	protected String path = null;
+	protected InclusionDependencyResultReceiver resultReceiver;
+
+	public List<ConfigurationSpecification> getConfigurationRequirements() {
+		List <ConfigurationSpecification> configurationSpecification = new ArrayList<ConfigurationSpecification>();
+		
+		configurationSpecification.add(new ConfigurationSpecificationString("pathToInputFile"));
+		configurationSpecification.add(new ConfigurationSpecificationCsvFile("input file"));
+		
+		return configurationSpecification;
+	}
+
+	public void start() {
+		if (path != null) {
+			try {
+				resultReceiver.receiveResult(
+						new ColumnCombination(
+								new ColumnIdentifier("table1", "column1"), 
+								new ColumnIdentifier("table1", "column2")),
+						new ColumnCombination(
+								new ColumnIdentifier("table2", "column3"),
+								new ColumnIdentifier("table2", "column2")));
+			} catch (CouldNotReceiveResultException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+	}
+
+	public void setResultReceiver(InclusionDependencyResultReceiver resultReceiver) {
+		this.resultReceiver = resultReceiver;
+	}
+
+	public void setConfigurationValue(String identifier, String value) {
+		if (identifier.equals("pathToInputFile")) {
+			path = value;
+		}		
+	}
+
+	public void setConfigurationValue(String identifier, boolean value) {
+		throw new UnsupportedOperationException();		
+	}
+
+	public void setConfigurationValue(String identifier, CsvFileGenerator value) {
+		throw new UnsupportedOperationException();		
+	}
+}
