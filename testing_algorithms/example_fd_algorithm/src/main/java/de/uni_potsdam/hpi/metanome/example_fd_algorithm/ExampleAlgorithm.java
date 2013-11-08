ba5b22edc9430114a041e2c6882765fab8fa2e9a
Claudia Exeler <cl.ex@gmx.de>
2013-11-08 16:24:01 +0100

    add unimplemented method (setting configuration value for SQL input) to example algorithms

--- testing_algorithms/example_fd_algorithm/src/main/java/de/uni_potsdam/hpi/metanome/example_fd_algorithm/ExampleAlgorithm.java
+++ testing_algorithms/example_fd_algorithm/src/main/java/de/uni_potsdam/hpi/metanome/example_fd_algorithm/ExampleAlgorithm.java
@@ -9,6 +9,7 @@ import de.uni_potsdam.hpi.metanome.algorithm_integration.FunctionalDependencyAlg
 import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecification;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecificationCsvFile;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecificationString;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.input.SQLInputGenerator;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.input.SimpleRelationalInputGenerator;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.CouldNotReceiveResultException;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.FunctionalDependencyResultReceiver;
@@ -58,6 +59,10 @@ public class ExampleAlgorithm implements FunctionalDependencyAlgorithm {
 
 	public void setConfigurationValue(String identifier, SimpleRelationalInputGenerator value) {
 		throw new UnsupportedOperationException();	
+	}
+
+	public void setConfigurationValue(String identifier, SQLInputGenerator value) {
+		throw new UnsupportedOperationException();				
 	}
 
 }
