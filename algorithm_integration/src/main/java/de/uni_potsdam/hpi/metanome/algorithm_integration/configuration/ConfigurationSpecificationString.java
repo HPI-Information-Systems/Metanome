e0369836bce57112ab7bf4a16a23dc023d8258e1
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-11-14 14:32:09 +0100

    removed Serializable interface from ConfigurationSpecificationString

--- algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/configuration/ConfigurationSpecificationString.java
+++ algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/configuration/ConfigurationSpecificationString.java
@@ -1,15 +1,11 @@
 package de.uni_potsdam.hpi.metanome.algorithm_integration.configuration;
 
-import java.io.Serializable;
-
 /**
  * Concrete {@link ConfigurationSpecification} for strings.
  * 
  * @see ConfigurationSpecification
  */
-public class ConfigurationSpecificationString extends ConfigurationSpecification implements Serializable{
-
-	private static final long serialVersionUID = 6081887975370271315L;
+public class ConfigurationSpecificationString extends ConfigurationSpecification {
 
 	/**
 	 * Construct a ConfigurationSepcificationString.
