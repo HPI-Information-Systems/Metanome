b2861ca0bc240c0df0bd1742100d109c020a8682
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-11-28 13:24:35 +0100

    added docs to Result

--- algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/results/Result.java
+++ algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/results/Result.java
@@ -2,8 +2,18 @@ package de.uni_potsdam.hpi.metanome.algorithm_integration.results;
 
 import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.OmniscientResultReceiver;
 
+/**
+ * @author Jakob Zwiener
+ * 
+ * All Results need to be sendable to an {@link OmniscientResultReceiver}.
+ */
 public interface Result {
 
+	/**
+	 * Sends a result to an {@link OmniscientResultReceiver}.
+	 * 
+	 * @param resultReceiver
+	 */
 	public void sendResultTo(OmniscientResultReceiver resultReceiver);
 	
 }
