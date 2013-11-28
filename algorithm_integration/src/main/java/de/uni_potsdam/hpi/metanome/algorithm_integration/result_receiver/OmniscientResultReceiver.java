0c2c66e349cc8a757b7dfe1d945d495da5155c2a
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-11-28 13:21:11 +0100

    moved OmniscientResultReceiver to algorithm_integration

---
+++ algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/result_receiver/OmniscientResultReceiver.java
@@ -0,0 +1,6 @@
+package de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver;
+
+
+public interface OmniscientResultReceiver extends BasicStatisticsResultReceiver, FunctionalDependencyResultReceiver, InclusionDependencyResultReceiver, UniqueColumnCombinationResultReceiver {
+
+}
