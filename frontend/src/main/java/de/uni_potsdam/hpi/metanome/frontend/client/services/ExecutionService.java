2db69a451404c6fff04732c14de55b9151fa5e8b
Claudia Exeler <cl.ex@gmx.de>
2013-11-28 14:16:51 +0100

    switch from sending String results to client to using Result object

--- frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/services/ExecutionService.java
+++ frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/services/ExecutionService.java
@@ -7,6 +7,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import de.uni_potsdam.hpi.metanome.algorithm_integration.AlgorithmConfigurationException;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.AlgorithmExecutionException;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.results.Result;
 import de.uni_potsdam.hpi.metanome.algorithm_loading.AlgorithmLoadingException;
 import de.uni_potsdam.hpi.metanome.frontend.client.parameter.InputParameter;
 
@@ -15,5 +16,5 @@ public interface ExecutionService extends RemoteService {
 
 	public long executeAlgorithm(String algorithmName, List<InputParameter> parameters) throws AlgorithmConfigurationException, AlgorithmLoadingException, AlgorithmExecutionException;
 
-	public List<String> fetchNewResults(String algorithmName);
+	public List<Result> fetchNewResults(String algorithmName);
 }
