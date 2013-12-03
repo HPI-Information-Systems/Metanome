5e79ab4114089b350af6b5c757b19705bcfcdcfd
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-12-03 14:52:55 +0100

    made AlgorithmConfigurationException throwable in algorithm execution

--- algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/AlgorithmConfigurationException.java
+++ algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/AlgorithmConfigurationException.java
@@ -1,6 +1,6 @@
 package de.uni_potsdam.hpi.metanome.algorithm_integration;
 
-public class AlgorithmConfigurationException extends Exception {
+public class AlgorithmConfigurationException extends AlgorithmExecutionException {
 
 	private static final long serialVersionUID = 187522613654685926L;
 	
