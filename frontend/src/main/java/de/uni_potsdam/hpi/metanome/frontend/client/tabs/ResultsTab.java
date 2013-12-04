811b5fc25cf3e8c13da50cae752d9a4a95ed0cef
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2013-12-04 13:27:14 +0100

    polling every 10 seconds

--- frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/tabs/ResultsTab.java
+++ frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/tabs/ResultsTab.java
@@ -56,7 +56,7 @@ public class ResultsTab extends DockPanel implements OmniscientResultReceiver {
 	      }
 	    };
 
-	    this.timer.scheduleRepeating(1000);
+	    this.timer.scheduleRepeating(10000);
 	}
 	
 	public AsyncCallback<Long> getCancelCallback() {
