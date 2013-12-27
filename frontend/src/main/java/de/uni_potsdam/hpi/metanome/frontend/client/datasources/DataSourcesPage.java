0835cdb1c74839f60072887ee14ad813cf89291a
Claudia Exeler <cl.ex@gmx.de>
2013-12-27 16:53:17 +0100

    create list structure in data sources page

--- frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/datasources/DataSourcesPage.java
+++ frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/datasources/DataSourcesPage.java
@@ -1,5 +1,7 @@
 package de.uni_potsdam.hpi.metanome.frontend.client.datasources;
 
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -11,10 +13,43 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  */
 public class DataSourcesPage extends VerticalPanel {
 
-	public DataSourcesPage() {
+	private FlexTable dbConnectionsList;
+	private FlexTable csvFilesList;
+
+	public DataSourcesPage() {		
+		createDataSourcesList();
+		
 		Label temporaryContent = new Label();
 		temporaryContent.setText("As data sources, you can configure any database connection in the Run Configurations,"
 				+ "or choose from any CSV-files in the designated folder.");
 		this.add(temporaryContent);
+	}
+
+	/**
+	 * This method triggers retrieval of all available data sources and returns a widget with a list of them.
+	 * @return
+	 */
+	private void createDataSourcesList() {
+		this.add(new HTML("<b>CSV Files</b>"));
+		csvFilesList = new FlexTable();
+		this.add(csvFilesList);
+		listCsvFiles();
+		
+		this.add(new HTML("<hr><b>Database Connections</b>"));
+		dbConnectionsList = new FlexTable();
+		this.add(dbConnectionsList);
+		listDbConnections();
+
+		this.add(new HTML("<hr>"));
+	}
+
+	private void listCsvFiles() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void listDbConnections() {
+		// TODO Auto-generated method stub
+		
 	}
 }
