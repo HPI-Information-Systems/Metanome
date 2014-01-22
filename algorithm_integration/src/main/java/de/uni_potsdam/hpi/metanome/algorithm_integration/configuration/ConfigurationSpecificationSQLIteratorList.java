a0b75efaa8cf4a64d61e21b7e05e4f54bd36ec3f
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2014-01-22 11:16:17 +0100

    implemented ConfigurationSpecificationSQLIteratorList

---
+++ algorithm_integration/src/main/java/de/uni_potsdam/hpi/metanome/algorithm_integration/configuration/ConfigurationSpecificationSQLIteratorList.java
@@ -0,0 +1,21 @@
+package de.uni_potsdam.hpi.metanome.algorithm_integration.configuration;
+
+/**
+ * Concrete {@link ConfigurationSpecification} for sql iterator lists.
+ * 
+ * @see ConfigurationSpecification
+ * 
+ * @author Jakob Zwiener
+ */
+public class ConfigurationSpecificationSQLIteratorList extends ConfigurationSpecification {
+
+	
+	/**
+	 * Construct a ConfigurationSpecificationSQLIteratorList.
+	 * 
+	 * @param a unique configuration parameter identifier
+	 */
+	public ConfigurationSpecificationSQLIteratorList(String identifier) {
+		super(identifier);
+	}
+}
---
+++ algorithm_integration/src/test/java/de/uni_potsdam/hpi/metanome/algorithm_integration/configuration/ConfigurationSpecificationSQLIteratorListTest.java
@@ -0,0 +1,46 @@
+package de.uni_potsdam.hpi.metanome.algorithm_integration.configuration;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for {@link ConfigurationSpecificationSQLIteratorList}
+ */
+public class ConfigurationSpecificationSQLIteratorListTest {
+
+	/**
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	/**
+	 * The identifier should be set in the constructor and be retrievable through getIdentifier.
+	 */
+	@Test
+	public void testGetIdentifier() {
+		// Setup
+		// Expected values
+		String expectedIdentifier = "parameter1";
+		ConfigurationSpecificationSQLIteratorList configSpec = new ConfigurationSpecificationSQLIteratorList(expectedIdentifier);
+		
+		// Execute functionality
+		String actualIdentifier = configSpec.getIdentifier();
+		
+		// Check result
+		assertEquals(expectedIdentifier, actualIdentifier);
+	}
+}
