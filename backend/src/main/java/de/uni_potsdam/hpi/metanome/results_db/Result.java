e9accdd801da9ab794368900f351856344e60235
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2014-04-19 14:35:31 +0200

    #4 added Results in database

---
+++ backend/src/main/java/de/uni_potsdam/hpi/metanome/results_db/Result.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2014 by the Metanome project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.uni_potsdam.hpi.metanome.results_db;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ * Represents a Result in the database.
+ *
+ * @author Jakob Zwiener
+ */
+@Entity
+public class Result {
+
+    protected String filePath;
+    protected Execution execution;
+    protected boolean isInd;
+    protected boolean isFd;
+    protected boolean isUcc;
+    protected boolean isBasicStat;
+
+    /**
+     * Exists for hibernate serialization
+     */
+    protected Result() {
+
+    }
+
+    /**
+     * @param filePath the path to the result file
+     */
+    public Result(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * Stores a result in the database.
+     *
+     * @param result the result to store
+     */
+    public static void store(Result result) throws EntityStorageException {
+        HibernateUtil.store(result);
+    }
+
+    /**
+     * Retrieves a result from the databse.
+     *
+     * @param filePath the result's file path.
+     * @return the result
+     */
+    public static Result retrieve(String filePath) throws EntityStorageException {
+        return (Result) HibernateUtil.retrieve(Result.class, filePath);
+    }
+
+    @Id
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @ManyToOne(targetEntity = Execution.class)
+    public Execution getExecution() {
+        return execution;
+    }
+
+    public void setExecution(Execution execution) {
+        this.execution = execution;
+    }
+
+    public boolean isInd() {
+        return isInd;
+    }
+
+    public void setInd(boolean isInd) {
+        this.isInd = isInd;
+    }
+
+    public boolean isFd() {
+        return isFd;
+    }
+
+    public void setFd(boolean isFd) {
+        this.isFd = isFd;
+    }
+
+    public boolean isUcc() {
+        return isUcc;
+    }
+
+    public void setUcc(boolean isUcc) {
+        this.isUcc = isUcc;
+    }
+
+    public boolean isBasicStat() {
+        return isBasicStat;
+    }
+
+    public void setBasicStat(boolean isBasicStat) {
+        this.isBasicStat = isBasicStat;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Result)) return false;
+
+        Result result = (Result) o;
+
+        if (isBasicStat != result.isBasicStat) return false;
+        if (isFd != result.isFd) return false;
+        if (isInd != result.isInd) return false;
+        if (isUcc != result.isUcc) return false;
+        if (execution != null ? !execution.equals(result.execution) : result.execution != null) return false;
+        if (!filePath.equals(result.filePath)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = filePath.hashCode();
+        result = 31 * result + (execution != null ? execution.hashCode() : 0);
+        result = 31 * result + (isInd ? 1 : 0);
+        result = 31 * result + (isFd ? 1 : 0);
+        result = 31 * result + (isUcc ? 1 : 0);
+        result = 31 * result + (isBasicStat ? 1 : 0);
+        return result;
+    }
+}
---
+++ backend/src/test/java/de/uni_potsdam/hpi/metanome/results_db/ResultTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2014 by the Metanome project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.uni_potsdam.hpi.metanome.results_db;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for {@link de.uni_potsdam.hpi.metanome.results_db.Result}
+ *
+ * @author Jakob Zwiener
+ */
+public class ResultTest {
+
+    /**
+     * Test method for {@link Result#store(Result)} and {@link Result#retrieve(String)}
+     * <p/>
+     * Tests persistence of a Result without attached {@link Execution}.
+     */
+    @Test
+    public void testPersistence() throws EntityStorageException {
+        // Setup
+        HibernateUtil.clear();
+
+        // Expected values
+        String expectedFilePath = "some file name";
+        Result expectedResult = new Result(expectedFilePath);
+
+        // Execute functionality
+        Result.store(expectedResult);
+        Result actualResult = Result.retrieve(expectedFilePath);
+
+        // Check result
+        assertEquals(expectedResult, actualResult);
+
+        // Cleanup
+        HibernateUtil.clear();
+    }
+
+    /**
+     * Test method for {@link Result#equals(Object)} and {@link de.uni_potsdam.hpi.metanome.results_db.Result#hashCode()}
+     */
+    @Test
+    public void testEqualsAndHashCode() {
+        // Setup
+        String expectedFilePath = "some file path";
+        Result result = new Result(expectedFilePath);
+        Result equalResult = new Result(expectedFilePath);
+        Result notEqualResult = new Result("some other path");
+
+        // Execute functionality
+        // Check result
+
+        // Reflexivity
+        assertEquals(result, result);
+        assertEquals(result.hashCode(), result.hashCode());
+
+        assertNotSame(result, equalResult);
+        assertEquals(result, equalResult);
+        assertEquals(result.hashCode(), equalResult.hashCode());
+
+        assertNotEquals(result, null);
+
+        assertNotEquals(result, notEqualResult);
+        assertNotEquals(result.hashCode(), notEqualResult.hashCode());
+    }
+
+}
