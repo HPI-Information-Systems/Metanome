beefb3b47ab5a64bc921e2b57f45f6deb60a7203
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2014-05-08 13:23:59 +0200

    #27 #71 added test algorithm (basic  statistic) that takes FileInputGenerators as input

---
+++ testing_algorithms/example_basic_stat_algorithm/src/main/java/de/uni_potsdam/hpi/metanome/algorithms/algorithm_template/BasicStatAlgorithm.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2014 by the Metanome project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.uni_potsdam.hpi.metanome.algorithms.algorithm_template;
+
+import de.uni_potsdam.hpi.metanome.algorithm_integration.AlgorithmConfigurationException;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.AlgorithmExecutionException;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnIdentifier;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.algorithm_types.BasicStatisticsAlgorithm;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.algorithm_types.FileInputParameterAlgorithm;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecification;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecificationCsvFile;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.input.FileInputGenerator;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.BasicStatisticsResultReceiver;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.results.BasicStatistic;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * An example algorithm for testing that expects 5 {@link de.uni_potsdam.hpi.metanome.algorithm_integration.input.FileInputGenerator}s and sends a {@link de.uni_potsdam.hpi.metanome.algorithm_integration.results.BasicStatistic} to the result receiver.
+ *
+ * @author Jakob Zwiener
+ */
+public class BasicStatAlgorithm implements BasicStatisticsAlgorithm, FileInputParameterAlgorithm {
+
+    protected static final String INPUT_FILE_IDENTIFIER = "input file";
+    protected static final int NUMBER_OF_INPUT_FILES = 5;
+    protected static final String STATISTIC_NAME = "file name statistic";
+    protected static final ColumnIdentifier COLUMN_IDENTIFIER = new ColumnIdentifier("testTable", "testColumn");
+
+    protected BasicStatisticsResultReceiver resultReceiver = null;
+    protected FileInputGenerator[] inputs = null;
+
+    @Override
+    public List<ConfigurationSpecification> getConfigurationRequirements() {
+        List<ConfigurationSpecification> configuration = new LinkedList<>();
+
+        configuration.add(new ConfigurationSpecificationCsvFile(INPUT_FILE_IDENTIFIER, NUMBER_OF_INPUT_FILES));
+
+        return configuration;
+    }
+
+    @Override
+    public void execute() throws AlgorithmExecutionException {
+        if (resultReceiver == null) {
+            throw new AlgorithmExecutionException("No result receiver was set.");
+        }
+        if (inputs == null) {
+            throw new AlgorithmExecutionException("No inputs were set.");
+        }
+        if (inputs.length != NUMBER_OF_INPUT_FILES) {
+            throw new AlgorithmExecutionException("Wrong number of inputs set.");
+        }
+
+        String fileName = inputs[4].getInputFile().getName();
+
+        resultReceiver.receiveResult(new BasicStatistic(STATISTIC_NAME, fileName, COLUMN_IDENTIFIER));
+    }
+
+    @Override
+    public void setResultReceiver(BasicStatisticsResultReceiver resultReceiver) {
+        this.resultReceiver = resultReceiver;
+    }
+
+    @Override
+    public void setConfigurationValue(String identifier, FileInputGenerator... values) throws AlgorithmConfigurationException {
+        if (!identifier.equals(INPUT_FILE_IDENTIFIER)) {
+            throw new AlgorithmConfigurationException("Metanome attempted to set a file input with wrong identifier.");
+        }
+        if (values.length != NUMBER_OF_INPUT_FILES) {
+            throw new AlgorithmConfigurationException("Metanome attempted to set an incorrect number of file inputs.");
+        }
+        inputs = values;
+    }
+}
---
+++ testing_algorithms/example_basic_stat_algorithm/src/test/java/de/uni_potsdam/hpi/metanome/algorithms/algorithm_template/BasicStatAlgorithmTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2014 by the Metanome project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.uni_potsdam.hpi.metanome.algorithms.algorithm_template;
+
+import de.uni_potsdam.hpi.metanome.algorithm_integration.AlgorithmConfigurationException;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.AlgorithmExecutionException;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecification;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.configuration.ConfigurationSpecificationCsvFile;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.input.FileInputGenerator;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.BasicStatisticsResultReceiver;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.results.BasicStatistic;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.isA;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests for {@link de.uni_potsdam.hpi.metanome.algorithms.algorithm_template.BasicStatAlgorithm}
+ *
+ * @author Jakob Zwiener
+ */
+public class BasicStatAlgorithmTest {
+
+    protected BasicStatAlgorithm algorithm;
+
+    @Before
+    public void setUp() throws Exception {
+        algorithm = new BasicStatAlgorithm();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link BasicStatAlgorithm#getConfigurationRequirements()}
+     * <p/>
+     * The algorithm should request a csv input file.
+     */
+    @Test
+    public void testGetConfigurationRequirements() {
+        // Execute functionality
+        List<ConfigurationSpecification> actualConfiguration = algorithm.getConfigurationRequirements();
+
+        // Check result
+        assertThat(actualConfiguration, hasItem(isA(ConfigurationSpecificationCsvFile.class)));
+    }
+
+    /**
+     * Test method for {@link BasicStatAlgorithm#execute()}
+     * <p/>
+     * Execution should fail if no {@link de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.BasicStatisticsResultReceiver} has been set on the algorithm.
+     *
+     * @throws AlgorithmConfigurationException
+     */
+    @Test
+    public void testExecuteNoResultReceiver() throws AlgorithmConfigurationException {
+        // Setup
+        FileInputGenerator[] inputs = new FileInputGenerator[BasicStatAlgorithm.NUMBER_OF_INPUT_FILES];
+        for (int i = 0; i < BasicStatAlgorithm.NUMBER_OF_INPUT_FILES; i++) {
+            inputs[i] = mock(FileInputGenerator.class);
+        }
+        algorithm.setConfigurationValue(BasicStatAlgorithm.INPUT_FILE_IDENTIFIER, inputs);
+
+        // Execute functionality
+        // Check result
+        try {
+            algorithm.execute();
+            fail("No exception was thrown.");
+        } catch (AlgorithmExecutionException e) {
+            // Intentionally left blank
+        }
+    }
+
+    /**
+     * Test method for {@link BasicStatAlgorithm#execute()}
+     * <p/>
+     * Execution should fail if no {@link de.uni_potsdam.hpi.metanome.algorithm_integration.input.FileInputGenerator} has been set on the algorithm.
+     */
+    @Test
+    public void testExecuteNoInputs() throws AlgorithmConfigurationException {
+        // Setup
+        algorithm.setResultReceiver(mock(BasicStatisticsResultReceiver.class));
+
+        // Execute functionality
+        // Check result
+        try {
+            algorithm.execute();
+            fail("No exception was thrown.");
+        } catch (AlgorithmExecutionException e) {
+            // Intentionally left blank
+        }
+    }
+
+    /**
+     * Test method for {@link BasicStatAlgorithm#execute()}
+     * <p/>
+     * When properly configured the algorithm should send a statistic to the result receiver. The statistic should contain the last input's file name as value.
+     */
+    @Test
+    public void testExecute() throws AlgorithmExecutionException {
+        // Setup
+        BasicStatisticsResultReceiver resultReceiver = mock(BasicStatisticsResultReceiver.class);
+        algorithm.setResultReceiver(resultReceiver);
+        FileInputGenerator[] inputs = new FileInputGenerator[BasicStatAlgorithm.NUMBER_OF_INPUT_FILES];
+        for (int i = 0; i < BasicStatAlgorithm.NUMBER_OF_INPUT_FILES; i++) {
+            inputs[i] = mock(FileInputGenerator.class);
+        }
+        algorithm.setConfigurationValue(BasicStatAlgorithm.INPUT_FILE_IDENTIFIER, inputs);
+        // Expected values
+        String expectedFileName = "some file name";
+        when(inputs[4].getInputFile()).thenReturn(new File(expectedFileName));
+
+        // Execute functionality
+        algorithm.execute();
+
+        // Check result
+        verify(resultReceiver).receiveResult(new BasicStatistic(BasicStatAlgorithm.STATISTIC_NAME, expectedFileName, BasicStatAlgorithm.COLUMN_IDENTIFIER));
+    }
+
+    /**
+     * Test method for {@link de.uni_potsdam.hpi.metanome.algorithms.algorithm_template.BasicStatAlgorithm#setConfigurationValue(String, de.uni_potsdam.hpi.metanome.algorithm_integration.input.FileInputGenerator...)}
+     * <p/>
+     * No input should be settable with the wrong identifier.
+     */
+    @Test
+    public void testSetConfigurationValueFileInputGeneratorWrongIdentifier() {
+        // Execute functionality
+        // Check result
+        try {
+            algorithm.setConfigurationValue("some other identifier", mock(FileInputGenerator.class));
+            fail("No exception was thrown.");
+        } catch (AlgorithmConfigurationException e) {
+            // Intentionally left blank
+        }
+    }
+
+    /**
+     * Test method for {@link de.uni_potsdam.hpi.metanome.algorithms.algorithm_template.BasicStatAlgorithm#setConfigurationValue(String, de.uni_potsdam.hpi.metanome.algorithm_integration.input.FileInputGenerator...)}
+     * <p/>
+     * 5 {@link de.uni_potsdam.hpi.metanome.algorithm_integration.input.FileInputGenerator}s should be set on the algorithm.
+     */
+    @Test
+    public void testSetConfigurationValueFileInputGeneratorWrongNumber() {
+        // Execute functionality
+        // Check result
+        try {
+            algorithm.setConfigurationValue(BasicStatAlgorithm.INPUT_FILE_IDENTIFIER, mock(FileInputGenerator.class));
+            fail("No exception was thrown.");
+        } catch (AlgorithmConfigurationException e) {
+            // Intentionally left blank
+        }
+    }
+
+}
