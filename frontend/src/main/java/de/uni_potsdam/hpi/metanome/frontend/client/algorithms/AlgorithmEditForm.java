c0a8950a78a4037581b49d2b7169787e95f07458
Claudia Exeler <cl.ex@gmx.de>
2014-07-04 10:53:37 +0200

    #38 UI element for algorithm adding

---
+++ frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/algorithms/AlgorithmEditForm.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2014 by the Metanome project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package de.uni_potsdam.hpi.metanome.frontend.client.algorithms;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * @author Claudia Exeler
+ * 
+ */
+public class AlgorithmEditForm extends FlowPanel {
+
+  protected TextBox fileNameTextBox = new TextBox();
+  protected TextBox nameTextBox = new TextBox();
+  protected TextBox authorTextBox = new TextBox();
+  protected TextBox descriptionTextBox = new TextBox();
+  protected CheckBox indCheckBox = new CheckBox("Inclusion Dependencies");
+  protected CheckBox fdCheckBox = new CheckBox("Functional Dependencies");
+  protected CheckBox uccCheckBox = new CheckBox("Unique Column Combinations");
+  protected CheckBox basicStatsCheckBox = new CheckBox("Basic Statistics");
+
+  public AlgorithmEditForm() {
+    this.add(new HTML("<h3>Add A New Algorithm</h3>"));
+    this.add(new Label(
+        "First, make your JAR file available by putting it in the algorithms folder."));
+    this.add(createGrid());
+    this.add(createSubmitButton());
+  }
+
+
+  private Grid createGrid() {
+    Grid grid = new Grid(5, 2);
+
+    grid.setText(0, 0, "File Name");
+    grid.setWidget(0, 1, this.fileNameTextBox);
+
+    grid.setText(1, 0, "Algorithm Name");
+    grid.setWidget(1, 1, this.nameTextBox);
+
+    grid.setText(2, 0, "Author");
+    grid.setWidget(2, 1, this.authorTextBox);
+
+    grid.setText(3, 0, "Description");
+    grid.setWidget(3, 1, this.descriptionTextBox);
+
+    grid.setText(4, 0, "Features");
+    VerticalPanel p = new VerticalPanel();
+    p.add(this.indCheckBox);
+    p.add(this.fdCheckBox);
+    p.add(this.uccCheckBox);
+    p.add(this.basicStatsCheckBox);
+    grid.setWidget(4, 1, p);
+    return grid;
+  }
+
+  /**
+   * @return
+   */
+  private Widget createSubmitButton() {
+    return new Button("Save");
+  }
+
+}
--- frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/algorithms/AlgorithmsPage.java
+++ frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/algorithms/AlgorithmsPage.java
@@ -1,26 +1,30 @@
 /*
  * Copyright 2014 by the Metanome project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package de.uni_potsdam.hpi.metanome.frontend.client.algorithms;
+
+import java.util.Collections;
+import java.util.List;
 
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.uni_potsdam.hpi.metanome.frontend.client.BasePage;
 import de.uni_potsdam.hpi.metanome.frontend.client.TabContent;
@@ -29,130 +33,134 @@ import de.uni_potsdam.hpi.metanome.frontend.client.services.FinderService;
 import de.uni_potsdam.hpi.metanome.frontend.client.services.FinderServiceAsync;
 import de.uni_potsdam.hpi.metanome.results_db.Algorithm;
 
-import java.util.Collections;
-import java.util.List;
-
 public class AlgorithmsPage extends VerticalPanel implements TabContent {
 
-    protected final FinderServiceAsync finderService;
-    protected final BasePage basePage;
-    private final FlexTable uccList;
-    private final FlexTable fdList;
-    private final FlexTable indList;
-    private final FlexTable statsList;
-    protected TabWrapper errorReceiver;
+  protected final FinderServiceAsync finderService;
+  protected final BasePage basePage;
+  private final FlexTable uccList;
+  private final FlexTable fdList;
+  private final FlexTable indList;
+  private final FlexTable statsList;
+  protected TabWrapper errorReceiver;
 
-    public AlgorithmsPage(BasePage parent) {
-        this.setWidth("100%");
-        this.setSpacing(5);
+  public AlgorithmsPage(BasePage parent) {
+    this.setWidth("100%");
+    this.setSpacing(5);
 
-        this.finderService = GWT.create(FinderService.class);
-        this.basePage = parent;
+    this.finderService = GWT.create(FinderService.class);
+    this.basePage = parent;
 
-        this.add(new HTML("<h3>Unique Column Combinations</h3>"));
-        this.uccList = new FlexTable();
-        this.add(this.uccList);
-        listAndAddUccAlgorithms();
+    this.add(new HTML("<h3>Unique Column Combinations</h3>"));
+    this.uccList = new FlexTable();
+    this.add(this.uccList);
+    listAndAddUccAlgorithms();
 
-        this.add(new HTML("<h3>Functional Dependencies</h3>"));
-        this.fdList = new FlexTable();
-        this.add(this.fdList);
-        listFdAlgorithms();
+    this.add(new HTML("<h3>Functional Dependencies</h3>"));
+    this.fdList = new FlexTable();
+    this.add(this.fdList);
+    listFdAlgorithms();
 
-        this.add(new HTML("<h3>Inclusion Dependencies<//h3>"));
-        this.indList = new FlexTable();
-        this.add(this.indList);
-        listIndAlgorithms();
+    this.add(new HTML("<h3>Inclusion Dependencies<//h3>"));
+    this.indList = new FlexTable();
+    this.add(this.indList);
+    listIndAlgorithms();
 
-        this.add(new HTML("<h3>Basic Statistics</h3>"));
-        this.statsList = new FlexTable();
-        this.add(this.statsList);
-        listStatsAlgorithms();
+    this.add(new HTML("<h3>Basic Statistics</h3>"));
+    this.statsList = new FlexTable();
+    this.add(this.statsList);
+    listStatsAlgorithms();
 
-        Label temporaryAddContent = new Label();
-        temporaryAddContent.setText("To add a new algorithm, put its jar in the designated folder.");
-        this.add(temporaryAddContent);
-    }
+    this.add(new HTML("<hr>"));
+    this.add(new AlgorithmEditForm());
+    // Label temporaryAddContent = new Label();
+    // temporaryAddContent
+    // .setText("To add a new algorithm, put its jar in the designated folder and provide details in this form:");
+    // this.add(temporaryAddContent);
+  }
 
-    /**
-     * Request a list of available UCC algorithms and display them in the uccList
-     */
-    private void listAndAddUccAlgorithms() {
-        finderService.listUniqueColumnCombinationsAlgorithms(getCallback(this.uccList));
-    }
+  /**
+   * Request a list of available UCC algorithms and display them in the uccList
+   */
+  private void listAndAddUccAlgorithms() {
+    finderService.listUniqueColumnCombinationsAlgorithms(getCallback(this.uccList));
+  }
 
-    /**
-     * Request a list of available FD algorithms and display them in the fdList
-     */
-    private void listFdAlgorithms() {
-        finderService.listFunctionalDependencyAlgorithms(getCallback(this.fdList));
-    }
+  /**
+   * Request a list of available FD algorithms and display them in the fdList
+   */
+  private void listFdAlgorithms() {
+    finderService.listFunctionalDependencyAlgorithms(getCallback(this.fdList));
+  }
 
-    /**
-     * Request a list of available IND algorithms and  display them in the indList
-     */
-    private void listIndAlgorithms() {
-        finderService.listInclusionDependencyAlgorithms(getCallback(this.indList));
-    }
+  /**
+   * Request a list of available IND algorithms and display them in the indList
+   */
+  private void listIndAlgorithms() {
+    finderService.listInclusionDependencyAlgorithms(getCallback(this.indList));
+  }
 
-    /**
-     * Request a list of available Basic Statistics algorithms and display them in the statsList
-     */
-    private void listStatsAlgorithms() {
-        finderService.listBasicStatisticsAlgorithms(getCallback(this.statsList));
-    }
+  /**
+   * Request a list of available Basic Statistics algorithms and display them in the statsList
+   */
+  private void listStatsAlgorithms() {
+    finderService.listBasicStatisticsAlgorithms(getCallback(this.statsList));
+  }
 
-    /**
-     * Constructs a callback that will add all results to the given table
-     *
-     * @param list Object that all returned elements will be added to
-     * @return the desired callback instance
-     */
-    protected AsyncCallback<List<Algorithm>> getCallback(final FlexTable list) {
-        return new AsyncCallback<List<Algorithm>>() {
-            public void onFailure(Throwable caught) {
-                errorReceiver.addError(caught.getMessage());
-                caught.printStackTrace();
-            }
+  /**
+   * Constructs a callback that will add all results to the given table
+   * 
+   * @param list Object that all returned elements will be added to
+   * @return the desired callback instance
+   */
+  protected AsyncCallback<List<Algorithm>> getCallback(final FlexTable list) {
+    return new AsyncCallback<List<Algorithm>>() {
+      public void onFailure(Throwable caught) {
+        errorReceiver.addError(caught.getMessage());
+        caught.printStackTrace();
+      }
 
-            public void onSuccess(List<Algorithm> result) {
-            	basePage.addAlgorithmsToRunConfigurations(result);
-                addAlgorithmsToList(result, list);
-            }
-        };
-    }
+      public void onSuccess(List<Algorithm> result) {
+        basePage.addAlgorithmsToRunConfigurations(result);
+        addAlgorithmsToList(result, list);
+      }
+    };
+  }
 
-    protected void addAlgorithmsToList(List<Algorithm> algorithms, FlexTable list) {
-        int row = list.getRowCount();
-        Collections.sort(algorithms);
-        for (Algorithm algorithm : algorithms) {
-            //Using the HTML title to associate an algorithm with each button.
-            Button runButton = new Button("Run");
-            runButton.setTitle(algorithm.getName());
-            runButton.addClickHandler(new ClickHandler() {
-                @Override
-                public void onClick(ClickEvent event) {
-                    jumpToRunConfiguration(((Button) event.getSource()).getTitle());
-                }
-            });
-
-            list.setHTML(row, 0, "<b>" + algorithm.getName() + "</b>");
-            list.setText(row, 1, "Author: " + algorithm.getAuthor());
-            list.setText(row, 2, "File: " + algorithm.getFileName());
-            list.setWidget(row, 3, runButton);
-            row++;
+  protected void addAlgorithmsToList(List<Algorithm> algorithms, FlexTable list) {
+    int row = list.getRowCount();
+    Collections.sort(algorithms);
+    for (Algorithm algorithm : algorithms) {
+      // Using the HTML title to associate an algorithm with each button.
+      Button runButton = new Button("Run");
+      runButton.setTitle(algorithm.getName());
+      runButton.addClickHandler(new ClickHandler() {
+        @Override
+        public void onClick(ClickEvent event) {
+          jumpToRunConfiguration(((Button) event.getSource()).getTitle());
         }
-    }
+      });
 
-    protected void jumpToRunConfiguration(String algorithmName) {
-        basePage.jumpToRunConfiguration(algorithmName, null);
+      list.setHTML(row, 0, "<b>" + algorithm.getName() + "</b>");
+      list.setText(row, 1, "Author: " + algorithm.getAuthor());
+      list.setText(row, 2, "File: " + algorithm.getFileName());
+      list.setWidget(row, 3, runButton);
+      row++;
     }
+  }
 
-    /* (non-Javadoc)
-     * @see de.uni_potsdam.hpi.metanome.frontend.client.TabContent#setErrorReceiver(de.uni_potsdam.hpi.metanome.frontend.client.TabWrapper)
-     */
-    @Override
-    public void setErrorReceiver(TabWrapper tab) {
-        this.errorReceiver = tab;
-    }
+  protected void jumpToRunConfiguration(String algorithmName) {
+    basePage.jumpToRunConfiguration(algorithmName, null);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * de.uni_potsdam.hpi.metanome.frontend.client.TabContent#setErrorReceiver(de.uni_potsdam.hpi.
+   * metanome.frontend.client.TabWrapper)
+   */
+  @Override
+  public void setErrorReceiver(TabWrapper tab) {
+    this.errorReceiver = tab;
+  }
 }
