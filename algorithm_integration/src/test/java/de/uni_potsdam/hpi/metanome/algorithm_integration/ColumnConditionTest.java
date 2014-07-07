53344762b15d7128038aabf434c6c80d40a41f73
jens.hildebrandt <jens.hildebrandt@student.hpi.uni-potsdam.de>
2014-07-07 12:15:51 +0200

    fixed comments in pull request for TestColumnCondition

--- algorithm_integration/src/test/java/de/uni_potsdam/hpi/metanome/algorithm_integration/ColumnConditionTest.java
+++ algorithm_integration/src/test/java/de/uni_potsdam/hpi/metanome/algorithm_integration/ColumnConditionTest.java
@@ -16,14 +16,14 @@
 
 package de.uni_potsdam.hpi.metanome.algorithm_integration;
 
+import de.uni_potsdam.hpi.metanome.test_helper.EqualsAndHashCodeTester;
+
 import org.junit.Test;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotSame;
 
 /**
  * Tests for {@link de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnCondition}
@@ -38,9 +38,9 @@ public class ColumnConditionTest {
    * and {@link ColumnCondition#hashCode()} <p/> {@link de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnCondition}
    * containing the same condition values for the same column should be equal
    */
-
   @Test
   public void testEqualsHashCode() {
+    //Setup
     ColumnIdentifier column11 = new ColumnIdentifier("table1", "column1");
     ColumnIdentifier column12 = new ColumnIdentifier("table2", "column2");
 
@@ -61,43 +61,28 @@ public class ColumnConditionTest {
 
     ColumnCondition
         columnCondition1 =
-        new ColumnCondition(column11,
-                            conditionValues1.toArray(new String[conditionValues1.size()]));
-
+        new ColumnCondition(column11, conditionValues1);
     ColumnCondition
         columnConditionEq =
-        new ColumnCondition(column21,
-                            conditionValues1.toArray(new String[conditionValues1.size()]));
+        new ColumnCondition(column21, conditionValues1);
     ColumnCondition
         columnConditionEq2 =
-        new ColumnCondition(column11,
-                            conditionValues2.toArray(new String[conditionValues2.size()]));
+        new ColumnCondition(column11, conditionValues2);
 
     ColumnCondition
         columnConditionNotEq =
-        new ColumnCondition(column12,
-                            conditionValues1.toArray(new String[conditionValues1.size()]));
+        new ColumnCondition(column12, conditionValues1);
     ColumnCondition
         columnConditionNotEq2 =
-        new ColumnCondition(column11,
-                            conditionValues3.toArray(new String[conditionValues3.size()]));
+        new ColumnCondition(column11, conditionValues3);
 
-    assertEquals(columnCondition1.hashCode(), columnCondition1.hashCode());
-    assertEquals(columnCondition1, columnCondition1);
+    EqualsAndHashCodeTester<ColumnCondition> tester = new EqualsAndHashCodeTester<>();
 
-    assertNotSame(columnCondition1, columnConditionEq);
-    assertEquals(columnCondition1.hashCode(), columnConditionEq.hashCode());
-    assertEquals(columnCondition1, columnConditionEq);
-
-    assertNotSame(columnCondition1, columnConditionEq2);
-    assertEquals(columnCondition1.hashCode(), columnConditionEq2.hashCode());
-    assertEquals(columnCondition1, columnConditionEq2);
-
-    assertNotSame(columnCondition1, columnConditionNotEq);
-    assertNotEquals(columnCondition1, columnConditionNotEq);
-
-    assertNotSame(columnCondition1, columnConditionNotEq2);
-    assertNotEquals(columnCondition1, columnConditionNotEq2);
+    //Execute functionality
+    //Check Result
+    tester.performBasicEqualsAndHashCodeChecks(columnCondition1, columnConditionEq,
+                                               columnConditionNotEq, columnConditionNotEq2);
+    tester.performBasicEqualsAndHashCodeChecks(columnCondition1, columnConditionEq2);
   }
 
 
