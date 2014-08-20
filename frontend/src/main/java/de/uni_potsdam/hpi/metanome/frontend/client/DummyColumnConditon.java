1d374a9d47d79290a8a906de928156d21787a683
Jens Hildebrandt <jens.hildebrandt@student.hpi.uni-potsdam.de>
2014-08-20 14:07:29 +0200

    added javadocs to explain the dummy class

--- frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/DummyColumnConditon.java
+++ frontend/src/main/java/de/uni_potsdam/hpi/metanome/frontend/client/DummyColumnConditon.java
@@ -9,7 +9,10 @@ import de.uni_potsdam.hpi.metanome.algorithm_integration.ColumnConditionValue;
 import de.uni_potsdam.hpi.metanome.algorithm_integration.results.ConditionalUniqueColumnCombination;
 
 /**
- * Created by Jens on 18.08.2014.
+ * @author Jens Ehrlich
+ *
+ * This class is a dummy class which serves the sole purpose of whitelisting it's member classes for GWT serialization.
+ * Corresponding methods that use this class are required in {@link de.uni_potsdam.hpi.metanome.frontend.client.services.ExecutionService}, {@link de.uni_potsdam.hpi.metanome.frontend.client.services.ExecutionServiceAsync}, and {@link de.uni_potsdam.hpi.metanome.frontend.server.ExecutionServiceImpl}.
  */
 public class DummyColumnConditon implements IsSerializable {
 
@@ -18,6 +21,4 @@ public class DummyColumnConditon implements IsSerializable {
   private ColumnConditionOr conditonOr;
   private ColumnConditionAnd conditonAnd;
   private ConditionalUniqueColumnCombination combination;
-
-
 }
