a136bb42acea7d9dd8e8434cfdd76fb81da4d906
Tanja Bergmann <tabergma@gmail.com>
2014-09-11 11:57:20 +0200

    #199 Rename Run button -> Analyze

--- frontend/src/main/java/de/metanome/frontend/client/datasources/TableInputTab.java
+++ frontend/src/main/java/de/metanome/frontend/client/datasources/TableInputTab.java
@@ -128,7 +128,7 @@ public class TableInputTab extends FlowPanel implements TabContent {
       }
     });
 
-    Button runButton = new Button("Run");
+    Button runButton = new Button("Analyze");
     runButton.setTitle(String.valueOf(input.getId()));
     runButton.addClickHandler(new ClickHandler() {
       @Override
