9f4c15f27d3e46c8de65535ba51d1ff71b1b67b9
Carl Ambroselli <git@carl-ambroselli.de>
2015-06-01 09:39:54 +0200

    Add algorithm list

--- frontend/V2/src/app/new/new.controller.js
+++ frontend/V2/src/app/new/new.controller.js
@@ -2,5 +2,41 @@
 
 angular.module('v2')
   .controller('NewCtrl', function ($scope, $log) {
-
+    $scope.category = [
+      {
+        name: 'Unique Column Combinations',
+        algorithms: [
+          {
+            name: 'example_holistic_algorithm',
+            desc: 'Example description',
+          },
+          {
+            name: 'example_relational_input_algorithm',
+            desc: 'Example description',
+          },
+          {
+            name: 'example_ucc_algorithm',
+            desc: 'Example description'
+          }
+        ]
+      },
+      {
+        name: 'Conditional Unique Column Combinations',
+        algorithms: [
+          {
+            name: 'example_cucc_algorithm',
+            desc: 'Example description',
+          }
+        ]
+      },
+      {
+        name: 'Functional Dependencies',
+        algorithms: [
+          {
+            name: 'example_cucc_algorithm',
+            desc: 'Example description',
+          }
+        ]
+      }
+    ]
   });
