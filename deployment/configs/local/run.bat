0bd20334d553c1d543f47140f35e6ce68f4c9a71
Vincent Schwarzer <vincent.schwarzer@yahoo.de>
2016-11-04 13:20:53 +0100

    Fixed Shell / Batch file exit

--- deployment/configs/local/run.bat
+++ deployment/configs/local/run.bat
@@ -1,4 +1,3 @@
 
-start java -Xmx2g -jar webapp-runner.jar backend --port 8081
-start java -Xmx2g -jar webapp-runner.jar frontend
-
+@start /b java -Xmx2g -jar webapp-runner.jar backend --port 8081
+@start /b java -Xmx2g -jar webapp-runner.jar frontend
--- deployment/configs/local/run.sh
+++ deployment/configs/local/run.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+trap 'kill %1' SIGINT
 java -Xmx2g -jar webapp-runner.jar backend --port 8081  &
-java -Xmx2g -jar webapp-runner.jar frontend && fg
+java -Xmx2g -jar webapp-runner.jar frontend
