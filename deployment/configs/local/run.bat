c6b8ad53ca1145cfcae1d39fc84a574511568f42
Vincent Schwarzer <vincent.schwarzer@yahoo.de>
2016-12-07 17:19:44 +0100

    Fixed run scripts for local deployment

--- deployment/configs/local/run.bat
+++ deployment/configs/local/run.bat
@@ -1,3 +1,2 @@
-
-@start /b java -Xmx2g -jar webapp-runner.jar backend --port 8081
-@start /b java -Xmx2g -jar webapp-runner.jar frontend
+@start java -Xmx2g -jar webapp-runner.jar backend --port 8081
+@start java -Xmx2g -jar webapp-runner.jar frontend
