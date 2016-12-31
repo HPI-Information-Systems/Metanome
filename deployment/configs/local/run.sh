2d9b780734c6ac265e730ce5ca6afbd38091d330
Vincent Schwarzer <vincent.schwarzer@yahoo.de>
2016-12-31 19:45:55 +0100

    Small fix on run.sh for local deployment

--- deployment/configs/local/run.sh
+++ deployment/configs/local/run.sh
@@ -1,4 +1,4 @@
-echo﻿"This script does not support Microsoft Windows please use the run.bat script.\n"
+echo "This script does not support Microsoft Windows please use the run.bat script.\n"
 
 trap 'kill %1' SIGINT
 java -Xmx2g -jar webapp-runner.jar backend --port 8081  &
